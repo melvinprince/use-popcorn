14b720c5901afc3de8409f2c255805643ae8d8f6
Melvin <melvinmps11301@gmail.com>
2025-01-10 01:10:28 +0530

    a few updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
-// import App from './App.jsx'
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App.jsx";
+// import StarRating from "./StarRating";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating maxRating={8} defaultRating={5} />
-    {/* <App /> */}
+    {/* <StarRating maxRating={8} defaultRating={5} /> */}
+    <App />
   </StrictMode>
 );
